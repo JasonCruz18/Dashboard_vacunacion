1b8cea0eb6406703034430d403fff0e1815c6df7
branmora <59512596+branmora@users.noreply.github.com>
2021-02-21 17:46:31 -0500

    #1 working page

--- app/ui.R
+++ app/ui.R
@@ -1,4 +1,14 @@
 library(shiny)
+library(shinydashboard)
+library(data.table)
+library(plyr)
+library(dplyr)
+library(highcharter)
+library(readxl)
+library(dplyr)
+library(ggplot2)
+library(ggiraph)
+
 
 # UI
 shinyUI(fluidPage(
@@ -6,6 +16,8 @@ shinyUI(fluidPage(
     # Application title
     titlePanel("Dashboard_vacunacion"),
     fluidRow(
-        box("Aquí va un gráfico")
+        box(girafeOutput("grafico2_1")),
+        box(highchartOutput("grafico1")),
+        box(highchartOutput("grafico2_2"))
     )
 ))
