c3c1d42835b3ffdfc321745063074b7dfa361129
Jason10121998 <164468@unsaac.edu.pe>
2021-02-21 01:16:36 -0500

    #1 Jason Cruz

--- Script/Batería de gráficos 1.R
+++ Script/Batería de gráficos 1.R
@@ -20,7 +20,7 @@ library(ggiraph)
 #+++++++++++++++++++++++++++++
 
 # fans_react = fread("https://raw.githubusercontent.com/manasi1096/Highcharts-HIMYM/master/FansReact-HIMYM.csv")
-base1 <- read_excel("D:/DIRESA Cusco/Dashboard_vacunacion/data/base_provincias_18.xlsx")
+base1 <- read_excel("D:/DIRESA Cusco/Dashboard_vacunacion/data/base_provincias_20.xlsx")
 
 # Paleta de colores
 
@@ -59,7 +59,7 @@ highchart() %>%
       type = "area"),
     list(
       name = "Avance",
-      data = round(base1$maximo),
+      data = round(base1$MAXIMO),
       pointPlacement = "on",
       type = "line")) %>% 
    hc_tooltip(pointFormat = "{point.y}%")  
@@ -105,24 +105,6 @@ donut_plot <- ggplot(donut_data, aes(y = value, fill = type)) +
   theme_minimal()
 
 ggiraph(ggobj = donut_plot)
-
-
-highchart(width = 400, height = 400) %>% 
-  hc_chart(type = "solidgauge",backgroundColor = "#F0F0F0",marginTop = 50) %>% 
-  hc_title(text = "Activity",style = list(fontSize = "24px")) %>% 
-  hc_tooltip(borderWidth = 0,backgroundColor = 'none',shadow = FALSE,style = list(fontSize = '16px'),
-             pointFormat = '{series.name}<br><span style="font-size:2em; color: {point.color}; font-weight: bold">{point.y}%</span>',
-             positioner = JS("function (labelWidth, labelHeight) {return {x: 200 - labelWidth / 2,y: 180};}")) %>% 
-  hc_pane(startAngle = 0,endAngle = 360,
-          background = list(
-            list(outerRadius = '112%',innerRadius = '88%',backgroundColor = JS("Highcharts.Color('#F62366').setOpacity(0.1).get()"),borderWidth =  0),
-            list(outerRadius = '87%',innerRadius = '63%',backgroundColor = JS("Highcharts.Color('#9DFF02').setOpacity(0.1).get()"),borderWidth = 0),
-            list(outerRadius = '62%',innerRadius =  '38%',backgroundColor = JS("Highcharts.Color('#0CCDD6').setOpacity(0.1).get()"),borderWidth = 0))) %>% 
-  hc_yAxis(min = 0,max = 100,lineWidth = 0,tickPositions = list()) %>% 
-  hc_plotOptions(solidgauge = list(borderWidth = '34px',dataLabels = list(enabled = FALSE),linecap = 'round',stickyTracking = FALSE)) %>% 
-  hc_add_series(name = "Move",borderColor = JS("Highcharts.getOptions().colors[0]"),data = list(list(color = JS("Highcharts.getOptions().colors[0]"),radius = "100%",innerRadius = "100%",y = 80))) %>% 
-  hc_add_series(name = "Exercise",borderColor = JS("Highcharts.getOptions().colors[1]"),data = list(list(color = JS("Highcharts.getOptions().colors[1]"),radius = "75%",innerRadius = "75%",y = 65))) %>% 
-  hc_add_series(name = "Stand",borderColor = JS("Highcharts.getOptions().colors[2]"),data = list(list(color = JS("Highcharts.getOptions().colors[2]"),radius = "50%",innerRadius = "50%",y = 50)))
 
 
 # Gráfico 2.2. 
